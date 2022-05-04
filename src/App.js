3fe51e4fd733e0c89877e2237a252170b5fa3c42
J.Han <han@stipop.io>
2022-05-04 17:22:08 +0900

    new ui

--- src/App.js
+++ src/App.js
@@ -1,119 +1,205 @@
 import React, { useState, useEffect } from 'react';
+import styled from "styled-components";
 
 import { fabric } from 'fabric';
 
+const Box = styled.div`
+	width: 1920px;
+	height: 990px;
+	background-color: #ffffff;
+`;
+
+const Headers = styled.div`
+    width: 1920px;
+    height: 80px;
+`;
+
+const Logo = styled.div`
+    width: 102px;
+    height: 36px;
+    object-fit: contain;
+    background-image: url(/images/stipop-logo.png);
+    margin: 20px 0 24px 68px 
+`;
+
+const Title = styled.div`
+    width: 207px;
+    height: 27px;
+    font-family: Manrope-ExtraLight_;
+    font-size: 20px;
+    font-weight: 800;
+    color: #2b292d;
+    margin: 6px 0 0 68px
+`;
+
+const Contents = styled.div`
+    width: 630px;
+    height: 96px;
+    font-family: Manrope;
+    font-size: 18px;
+    font-weight: 600;
+    color: #2b292d;
+    margin: 12px 0 0 68px
+`;
+
+const Body = styled.div`
+    width: 100%;
+    align-items: center;
+    margin-top: 40px;
+`;
+
+const SearchBox = styled.div`
+    width: 614px;
+    height: 676px;
+    border-radius: 6px;
+    border: solid 1px #d9d9d9;
+    background-color: #ffffff;
+    display: inline-block;
+    float: left;
+    margin: 0 0 0 68px;
+    overflow:scroll;
+`;
+
+const CanvasArea = styled.div`
+    width: 1193px;
+    height: 676px;
+    border-radius: 6px;
+    border: solid 1px #2b292d;
+    background-color: #f9f9f9;
+    display: inline-block;
+    float: left;
+    margin: 0 0 0 20px;
+`;
+
+const SearchInput = styled.input`
+    width: 495px;
+    height: 58px;
+    object-fit: contain;
+    border-radius: 18px;
+    border: solid 1px #d9d9d9;
+    background-color: #ffffff;
+    margin: 16px 0 0 16px;
+    padding-left: 71px;
+    font-size: 24px;
+    line-height: 18px;
+    color: #343c4b;
+    background-image: url(/images/icon-copy.png);
+    background-repeat: no-repeat;
+    background-position: left;
+    background-position-x: 20px;
+`;
+
+const ImgList = styled.li`
+    float: left;
+    padding-right: 32px;
+`;
+
 const App = () => {
-  
-  const [canvas, setCanvas] = useState('');  
-  const [apikey, setApiKey] = useState('');
-  const [lang, setLang] = useState('en');
-  const [searchData, setSearchData] = useState([]);
-  const [searchText, setSearchText] = useState('');
+	
+	const [canvas, setCanvas] = useState('');  
+	const [apikey, setApiKey] = useState('');
+	const [lang, setLang] = useState('en');
+	const [searchData, setSearchData] = useState([]);
+	const [searchText, setSearchText] = useState('');
 
-  useEffect(() => {
-    setCanvas(initCanvas());
-  }, []);  
-  
-  const initCanvas = () => (
-    new fabric.Canvas('canvas', {
-      height: 1000,
-      width: 800,
-      backgroundColor: 'grey'
-    })
-  );  
+	useEffect(() => {
+		setCanvas(initCanvas());
+	}, []);  
+	
+	const initCanvas = () => (
+		new fabric.Canvas('canvas', {
+			width: 1193,
+			height: 676
+		})
+	);  
 
-  //변경된 후 코드
-  const addRect = (canvi, stickerImg) => {
-    
-    fabric.util.loadImage(`${stickerImg}`, img => { 
+	//변경된 후 코드
+	const addRect = (canvi, stickerImg) => {
+		
+		fabric.util.loadImage(`${stickerImg}`, img => { 
 
-      const imgInstance = new fabric.Image(img);
-      imgInstance.scale(0.5);
+			const imgInstance = new fabric.Image(img);
+			imgInstance.scale(0.5);
 
-      canvi.add(imgInstance).renderAll();
+			canvi.add(imgInstance).renderAll();
 
-    }, null, { crossorigin: 'Anonymous'});
-  }
+		}, null, { crossorigin: 'Anonymous'});
+	}
 
-  //변경전 코드
-  // const addRect = (canvi, stickerImg) => {
-  //   fabric.Image.fromURL(`${stickerImg}`, img => { 
-  //     img.scale(0.5);
-  //     canvi.add(img).renderAll();
-  //   });
-  // }
+	const search = async (searchText) => {
+		
+		setSearchText(searchText);
 
+		const response = await fetch(`https://messenger.stipop.io/v1/search/test?q=${searchText}&userId=9937&lang=en`, {
+			headers: {
+				'Content-Type': 'application/json',
+				'apikey' : `${process.env.API_KEY}`
+			}
+			,method: 'GET'
+		});
 
+		const data = await response.json();
 
-  const search = async (searchText) => {
-    
-    setSearchText(searchText);
+		if (data.body.stickerList) {
+			setSearchData(data.body.stickerList);
+		} else {
+			setSearchData([]);
+		}
+		
+	}
 
-    const response = await fetch(`https://messenger.stipop.io/v1/search/test?q=${searchText}&userId=9937&lang=${lang === 'other' ? '':lang}&animated=N&limit=30`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'apikey' : `${apikey}`
-      }
-      ,method: 'GET'
-    });
+	const save = () => {
 
-    const data = await response.json();
+		const data = canvas.toDataURL({
+			format: 'png',
+			quality: 0.8
+		});
 
-    if (data.body.stickerList) {
-      setSearchData(data.body.stickerList);
-    } else {
-      setSearchData([]);
-    }
-    
-  }
+		const link = document.createElement('a');
 
-  const save = () => {
+		link.href = data;
+		link.download = 'image.png';
+		link.click();
 
-    const data = canvas.toDataURL({
-      format: 'png',
-      quality: 0.8
-    });
+	}
 
-    const link = document.createElement('a');
+	return(
+		<Box>
+            <Headers>
+                <Logo />
+            </Headers>
+            <Title>
+                Sticker Search Demo
+            </Title>
+            <Contents>
+                1) Search sticker using the search UI on the left <br />
+                2) Click on a sticker to add it to the board on the right<br />
+                3) Try moving & resizing stickers on the board<br />
+                4) Hover on the sticker from the search UI to find it’s sticker pack name
+            </Contents>
+            <Body>
+                <SearchBox>
+                    <SearchInput value={searchText} onChange={e => search(e.target.value)} placeholder="Search sticker.." />
+                    <ul style={{listStyle:'none'}}>
+                    {
+                        searchData.map((item, index) => {
 
-    link.href = data;
-    link.download = 'image.png';
-    link.click();
+                                return (
+                                    <ImgList key={item.seq} onClick={() => addRect(canvas, item.stickerImg)} >
+                                        <img style={{width:100, height:100}} src={item.stickerImg} />
+                                    </ImgList>
+                                )
 
-  }
-
-  return(
-    <>
-    <div style={{float:'left'}}>
-      <h1>Fabric.js Test</h1>
-      <canvas id="canvas" />
-    </div>
-    <div>
-      <h1>Search Sticker</h1>
-      <input type="Text" value={apikey} onChange={e => setApiKey(e.target.value)} placeholder="api key" />
-      <select value={lang} onChange={e => setLang(e.target.value)}>
-        <option value="en">English</option>
-        <option value="ko">Korean</option>
-        <option value="other">Other</option>
-      </select>
-      <input type="Text" value={searchText} onChange={e => search(e.target.value)} placeholder="search text" /><br/>
-      <button onClick={save} >다운로드</button><br/>
-      <ul style={{listStyle:'none'}}>
-        {
-          searchData.map((item, index) => {
-
-              return (
-                <li style={{float:'left'}} key={item.seq} onClick={() => addRect(canvas, item.stickerImg)}>
-                  <img style={{width:100, height:100}} src={item.stickerImg} />
-                </li>
-              )
-
-          })
-        }
-      </ul>
-    </div>
-    </>
-  );
+                        })
+                    }
+                    </ul>
+                </SearchBox>
+                <CanvasArea>
+                    <canvas id="canvas" />
+                </CanvasArea>
+            </Body>
+		</Box>
+	);
 }
 
 export default App;
